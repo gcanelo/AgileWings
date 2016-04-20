6dd9d2e52e7f53fc2482da82738dd0991bcbbbe3
Kleer LA <kleer@kleer-sandbox.(none)>
2016-04-20 16:08:41 -0300

    Segunda version de clase blackJack.rb

--- lib/blackJack.rb
+++ lib/blackJack.rb
@@ -2,4 +2,12 @@ class BlackJack
 	def bienvenida
 		return "Bienvenido a BlackJack"
 	end
+
+	def pedirCarta
+		return 5
+	end
+
+	def cargarBanca
+		return "18"
+	end
 end
--- spec/blackJack_spec.rb
+++ spec/blackJack_spec.rb
@@ -6,4 +6,14 @@ describe "BlackJack" do
 		game.bienvenida.should == "Bienvenido a BlackJack"
 	end
 
+	it "pedir carta" do
+		game = BlackJack.new
+		game.pedirCarta.should <= 5
+	end
+	
+	it "cargar Banca" do
+		game = BlackJack.new
+		game.cargarBanca.should == "18"
+	end
+
 end
